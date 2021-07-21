963d32a60757d699dc75c293292eabb0ee2d61aa
natalia <mellomartins.naty@gmail.com>
2021-07-21 19:39:46 -0300

    requisito 3 tentativa js

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-const emailInput = document.getElementById('login');
-const passwordInput = document.getElementById('senha');
-const loginButton = document.getElementById('buttonLogin');
+// const emailInput = document.getElementById('login');
+// const passwordInput = document.getElementById('senha');
+// const loginButton = document.getElementById('buttonLogin');
 // const emailTrybe = 'tryber@tester.com';
 // const senhaTrybe = '123456';
 
-loginButton.addEventListener('click', () => {
-  if (emailInput.value === 'tryber@tester.com' && passwordInput.value === '123456') {
-    alert('Olá, Tryber!');
-  } else {
-    alert('Login ou senha inválidos.');
-  }
-});
+// loginButton.addEventListener('click', () => {
+//   if (emailInput.value === 'tryber@tester.com' && passwordInput.value === '123456') {
+//     alert('Olá, Tryber!');
+//   } else {
+//     alert('Login ou senha inválidos.');
+//   }
+// });
 
 // function logarButton() {
-//   if (emailInput.value == 'tryber@tester.com' && passwordInput.value == '123456') {
+
+//   if (emailInput.value === 'tryber@tester.com' && passwordInput.value === '123456') {
 //     alert("Olá, Tryber!");
 //   } else {
 //     alert('Login ou senha inválidos.');
 //   }
 // };
-// // logarButton();
 
-// loginButton.addEventListener('click', logarButton);
+
+// window.onload = function () {
+//   const emailInput = document.getElementById('login');
+//   const passwordInput = document.getElementById('senha');
+//   const loginButton = document.getElementById('buttonLogin');
+//   function logarButton() {
+
+//     if (emailInput.value === 'tryber@tester.com' && passwordInput.value === '123456') {
+//       alert("Olá, Tryber!");
+//     } else {
+//       alert('Login ou senha inválidos.');
+//     }
+//   };
+//   loginButton.addEventListener('click', logarButton);
+// }
+
+window.onload = function () {
+  const emailInput = document.getElementById('login');
+  const passwordInput = document.getElementById('senha');
+  const loginButton = document.getElementById('buttonLogin');
+  const emailTrybe = 'tryber@tester.com';
+  const senhaTrybe = '123456';
+
+  loginButton.addEventListener('click', () => {
+    if (emailInput.value == emailTrybe && passwordInput.value == senhaTrybe) {
+      alert("Olá, Tryber!");
+    } else {
+      alert("Login ou senha inválidos.");
+    }
+  });
+}
